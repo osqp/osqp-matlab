c376640e28ecfd40c8d7b29fd201cbc3225ec84f
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-12-06 10:05:46 -0500

    Bump version: 0.4.1 → 0.5.0

--- osqp.m
+++ osqp.m
@@ -1,5 +1,5 @@
 classdef osqp < handle
-    % osqp interface class for OSQP solver v0.4.1
+    % osqp interface class for OSQP solver v0.5.0
     % This class provides a complete interface to the C implementation
     % of the OSQP solver.
     %
