034350218cd658b314ce7693669a6bc1556ba0d7
gbanjac <gbanjac@control.ee.ethz.ch>
2019-05-23 23:58:59 +0200

    Fixed tests when a problem is both primal and dual infeasible

--- run_osqp_tests.m
+++ run_osqp_tests.m
@@ -1,13 +1,11 @@
 import matlab.unittest.TestSuite;
-import matlab.unittest.constraints.IsLessThan;
 
 [osqp_path,~,~] = fileparts(which('osqp.m'));
 unittest_dir = fullfile(osqp_path, 'unittests');
 suiteFolder = TestSuite.fromFolder(unittest_dir);
 
 % Solve individual test file
-%suiteFolder = TestSuite.fromFile('unittests/non_cvx_tests.m');
-
+%suiteFolder = TestSuite.fromFile('unittests/dual_infeasibility_tests.m');
 
 % Run all suite
 result = run(suiteFolder);
