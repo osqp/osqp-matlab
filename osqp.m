5feb976374988d5534f2e1f9a059f36a8ee40da3
Vineet Bansal <vineetbansal@protonmail.com>
2021-09-03 11:12:58 -0400

    Update osqp.m
    For code generation on Mac OS X, the location usr/local/bin needs to be added to the PATH to find cmake installed through homebrew.

--- osqp.m
+++ osqp.m
@@ -530,6 +530,13 @@ classdef osqp < handle
 
             % Create project
             if ~isempty(project_type)
+	    
+                % Extend path for CMake mac (via Homebrew)
+		PATH = getenv('PATH');
+		if ((ismac) && (isempty(strfind(PATH, '/usr/local/bin'))))
+		    setenv('PATH', [PATH ':/usr/local/bin']);
+		end
+    
                 fprintf('Creating project...\t\t\t\t\t\t\t\t');
                 orig_dir = pwd;
                 cd(target_dir);
